66a281fe61a94ee40b492d30fd1e6e3bb0227ff6
shaafshahzad <shaaf.m.shahzad@gmail.com>
2025-09-26 21:32:58 -0400

    Change album details font size

--- components/albumInfo.tsx
+++ components/albumInfo.tsx
@@ -9,8 +9,8 @@ interface AlbumInfoProps {
 const AlbumInfo = ({ title, artist, album }: AlbumInfoProps) => {
 	return (
 		<>
-			<p className=" text-white font-semibold text-sm sm:text-xl">{title}</p>
-			<p className="text-white text-center text-opacity-50 font-medium text-sm sm:text-xl">
+			<p className=" text-white font-medium text-sm sm:text-xl">{title}</p>
+			<p className="text-white text-center text-opacity-50 font-medium text-sm sm:text-lg">
 				{artist} - {album}
 			</p>
 		</>
